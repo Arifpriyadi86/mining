88b7637f37183b56b6659e81d40e0fec88af08b8
Arifpriyadi86 <135079549+Arifpriyadi86@users.noreply.github.com>
2023-05-31 08:22:02 +0700

    Update Mining2.sh

--- Mining2.sh
+++ Mining2.sh
@@ -1,1 +1,10 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/release/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a m7m -o lapisan + tcp://m7m.asia.mine.zergpool.com:6033 -u TTKVZUMVN8B77fqvsgtnihAjfNHELXgpeC -p  c = TRX -t  8
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/release/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a m7m -o stratum+tcp://m7m.asia.mine.zergpool.com:6033 -u TTKVZUMVN8B77fqvsgtnihAjfNHELXgpeC -p c=TRX -t 8
+
+sc mining github
+
+wget https://raw.githubusercontent.com/Arifpriyadi86/mining/main/Mining2.sh
+
+chmod + Mining2.sh
+./Mining.sh
